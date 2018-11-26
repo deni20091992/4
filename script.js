16d66750223a6f0be16ae37feb4fda3a774b6f5f
Denis.Bashkirov <deni20091992@gmail.com>
2018-11-26 23:21:43 +0300

    Add zadanie4

---
+++ script.js
@@ -0,0 +1,37 @@
+    var list = document.querySelector('ul');
+    list.addEventListener('click', function (ev) {
+        if(ev.target.tagName === "LI") {
+            ev.target.classList.toggle('checked');
+        } else if(ev.target.tagName === "SPAN") {
+            var div = ev.target.parentNode;
+            div.remove();
+        }
+    }, false);
+    function newElement() {
+        var inputValue = document.getElementById('toDoList').value;
+        var inputData = document.getElementById('adddata').value;
+        var p = document.createTextNode(inputData + " | " + inputValue);
+        var li = document.createElement('li');
+        li.appendChild(p);
+        if (inputData == ""){
+            alert("Заполните дату")
+        }
+        else if (inputValue == ""){
+            alert("Введите дело")
+        }
+        else {
+            document.getElementById('list').appendChild(li);
+        }
+        document.getElementById('toDoList').value = "";
+    var span = document.createElement('SPAN');
+    var txt = document.createTextNode("Удалить");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    }
+    function delList() {
+        var li = document.getElementsByTagName('li');
+        for (var i = 0; i < 10; i++) {
+            li[i].style.display = 'none';
+        }
+    }
